8770c7e77a2ead3345f74208fe50bc17b7e96c4f
AtifWeb <mytechlife96@gmail.com>
2021-09-16 09:03:15 -0700

    new

--- assets/js/CustomAudioPlayer.js
+++ assets/js/CustomAudioPlayer.js
@@ -1,112 +1,100 @@
-export const AudioPlayer=(FileLink,Track)=>{
-    
-    let AudioButton=document.querySelector(".PlayButton");
-    let AudioRange=document.querySelector(".custom-range");
-    let isPlay=true
-    let isPlayed=false;
-  
-    Track.src=FileLink
-    let PlayInterval=""
+export const AudioPlayer = (FileLink, Track) => {
+  let AudioButton = document.querySelector(".PlayButton");
+  let AudioRange = document.querySelector(".range-div > input");
+  let isPlay = true;
+  let isPlayed = false;
 
-    let ClosePopupIcon=document.querySelector(".view-popup .close-icon");
+  Track.src = FileLink;
+  let PlayInterval = "";
 
-const GetCurrentTrackTimePercentage=(time,duration)=>{
-    return time*100/duration
+  let ClosePopupIcon = document.querySelector(".view-popup .close-icon");
 
-}
+  const GetCurrentTrackTimePercentage = (time, duration) => {
+    return (time * 100) / duration;
+  };
 
-const HandleDestroyObject=()=>{
-    
-    PauseAudio()
-    Track.currentTime=0
-    document.querySelector(".audio-area > .icon-wrapper").innerHTML=`  <i class="fas fa-headphones-alt"></i>`
-        
-    document.querySelector(".audio-area > .icon-wrapper").classList.remove("rotate")
+  const HandleDestroyObject = () => {
+    PauseAudio();
+    Track.currentTime = 0;
+    document.querySelector(
+      ".audio-area > .icon-wrapper"
+    ).innerHTML = `  <i class="fas fa-headphones-alt"></i>`;
 
-    clearInterval(PlayInterval)
+    document
+      .querySelector(".audio-area > .icon-wrapper")
+      .classList.remove("rotate");
 
-    isPlayed=false
+    clearInterval(PlayInterval);
 
+    isPlayed = false;
+  };
+  let d = 0;
+  setInterval(() => {
+    let CurrentTimePercentage = GetCurrentTrackTimePercentage(
+      Track.currentTime,
+      Track.duration
+    );
+    d = d + 5;
+    AudioRange.value = `${d}`;
+  }, 1000);
 
-}
+  AudioRange.addEventListener("mouseup", (e) => {
+    let CurrentRangePercentage = e.target.value;
+    Track.currentTime = (CurrentRangePercentage * Track.duration) / 100;
+    console.log(CurrentRangePercentage);
+  });
 
+  const PlayAudio = () => {
+    AudioButton.innerHTML = '<i class="fas fa-pause"></i>';
+    isPlay = false;
+    Track.play();
+  };
+  const PauseAudio = () => {
+    AudioButton.innerHTML = ' <i class="fas fa-play"></i>';
+    isPlay = true;
+    Track.pause();
+  };
 
-    setInterval(() => {
-  
-        let CurrentTimePercentage=GetCurrentTrackTimePercentage(Track.currentTime,Track.duration)
-        AudioRange.style.width=`${CurrentTimePercentage}%`
-    }, 1000);
+  const HandlePlayButton = (e) => {
+    let AudioButton = e.target;
 
+    if (isPlay == true) {
+      PlayAudio();
 
+      PlayInterval = setInterval(HandleCurrentState, 1000);
+    } else {
+      PauseAudio();
+    }
+  };
 
-    AudioRange.addEventListener("mouseup",(e)=>{
-        let CurrentRangePercentage=e.target.value;
-        Track.currentTime=CurrentRangePercentage*Track.duration/100
-        console.log(CurrentRangePercentage)
-        
-    })
-   
+  const HandleCurrentState = () => {
+    console.log("wir");
 
-const PlayAudio=()=>{
-    AudioButton.innerHTML='<i class="fas fa-pause"></i>'
-    isPlay=false
-    Track.play()
-}  
-const PauseAudio=()=>{
-    AudioButton.innerHTML=' <i class="fas fa-play"></i>'
-            isPlay=true
-            Track.pause()
-}  
+    if (Track.played.length == 0) {
+      document.querySelector(
+        ".audio-area > .icon-wrapper"
+      ).innerHTML = `<i class="fas fa-spinner"></i>   `;
 
+      document
+        .querySelector(".audio-area > .icon-wrapper")
+        .classList.add("rotate");
 
-    const HandlePlayButton=e=>{
-        
-        let AudioButton=e.target;
+      isPlayed = true;
+    } else {
+      document.querySelector(
+        ".audio-area > .icon-wrapper"
+      ).innerHTML = `  <i class="fas fa-headphones-alt"></i>`;
 
-        
-        if(isPlay==true){
-            PlayAudio()
-
-          PlayInterval=  setInterval(HandleCurrentState,1000)
-        }else{
-          
-            PauseAudio()
-           
-                
-          
-            
-        }
-
+      document
+        .querySelector(".audio-area > .icon-wrapper")
+        .classList.remove("rotate");
     }
 
-
-const HandleCurrentState=()=>{
-console.log("wir")
-  
-    if(Track.played.length==0){
-        document.querySelector(".audio-area > .icon-wrapper").innerHTML=`<i class="fas fa-spinner"></i>   `
-        
-        document.querySelector(".audio-area > .icon-wrapper").classList.add("rotate")
-
-        isPlayed=true
-
-    }else{
-        document.querySelector(".audio-area > .icon-wrapper").innerHTML=`  <i class="fas fa-headphones-alt"></i>`
-        
-        document.querySelector(".audio-area > .icon-wrapper").classList.remove("rotate")
+    if ((isPlayed = true)) {
+      clearInterval(PlayInterval);
     }
+  };
 
-    if(isPlayed=true){
-        clearInterval(PlayInterval)
-    }
-   
-
-
-
-}
-
-    AudioButton.addEventListener("click",HandlePlayButton)
-    ClosePopupIcon.addEventListener("click",HandleDestroyObject)
-}
-
-
+  AudioButton.addEventListener("click", HandlePlayButton);
+  ClosePopupIcon.addEventListener("click", HandleDestroyObject);
+};
